611712faddcd76cba1b62a7139864702f69a0218
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2016-07-08 21:49:40 +0200

    Codility - Arrays - CyclicRotation

---
+++ Codility.Tasks.Tests/Lesson2.Arrays/CyclicRotationTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codility.Tasks.Lesson2.Arrays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tasks.Lesson2.Arrays.Tests
+{
+    [TestClass]
+    public class RotationTests
+    {
+        [TestMethod]
+        public void solutionTest()
+        {
+            var sut = new CyclicRotation();
+            var input = new int[] { 3, 8, 9, 7, 6 };
+            var expected = new int[] { 9, 7, 6, 3, 8 };
+            var actual = sut.solution(input, 3);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Codility.Tasks/Lesson2.Arrays/CyclicRotation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tasks.Lesson2.Arrays
+{
+    public class CyclicRotation
+    {
+        public int[] solution(int[] A, int K)
+        {
+            if (K < 0) throw new ArgumentException(nameof(K));
+            if (A == null) throw new ArgumentNullException(nameof(A));
+            if (A.Length == 0) return A;
+            if (K == 0 || K == A.Length) return A;
+
+            K = K % A.Length;
+            var result = new int[A.Length];
+
+            Array.Copy(A, A.Length - K, result, 0, K);
+            Array.Copy(A, 0, result, K, A.Length - K);
+
+            return result;
+        }
+    }
+}
