a01871a0110d6a8f9d7b45023ba1b27bf3e67a6a
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2016-07-08 20:58:01 +0200

    Codility - initial commit + binaryGap task

---
+++ Codility.Tasks/BinaryGap.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tasks
+{
+    public class BinaryGap
+    {
+        public int solution(int N)
+        {
+            var max = 0;
+            var zeroCount = 0;
+            var hadOne = false;
+
+            while (N > 0)
+            {
+                if ((N & 1) == 1)
+                {
+                    max = Math.Max(max, zeroCount);
+                    zeroCount = 0;
+                    hadOne = true;
+                }
+                else
+                {
+                    if (hadOne) zeroCount++;
+                }
+
+                N >>= 1;
+            }
+
+            return max;
+        }
+    }
+}
