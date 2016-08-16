14d765f89ee8f4edab5f750eb6189ce6ebe24b42
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2016-08-16 14:35:03 +0200

    Lesson92 - SocksLaundering

---
+++ Codility.Tasks.Tests/Lesson92/SocksLaunderingTests.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codility.Tasks.Lesson92;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tasks.Lesson92.Tests
+{
+    [TestClass]
+    public class SocksLaunderingTests
+    {
+        private SocksLaundering sut = new SocksLaundering();
+
+        [TestMethod]
+        public void ShouldReturnCountOfClean_GivenNoSpaceInLaundry()
+        {
+            Scenario(new int[] { 1, 1, 2 }, new int[] { 2 }, 0, 1);
+        }
+
+        [TestMethod]
+        public void ShouldPairCleanWithDirty_GivenValidPairs()
+        {
+            Scenario(new int[] { 1, 2, 3 }, new int[] { 2, 3, 4 }, 2, 2);
+        }
+
+        [TestMethod]
+        public void ShouldReturnSum_GivenLotsOfSpaceInLaundry()
+        {
+            Scenario(new int[] { 1, 1, 2, 3, 4, 4 }, new int[] { 2, 3, 5, 5 }, 100, 5);
+        }
+
+        [TestMethod]
+        public void ShouldReturnCorrectNumber_GivenAllEvenPairsAndSufficientCapacity()
+        {
+            Scenario(new int[] { 1, 1, 2, 2 }, new int[] { 2, 2, 3, 3 }, 4, 4);
+        }
+
+        [TestMethod]
+        public void ShouldReturnCorrectNumber_GivenAllEvenPairsAndInSufficientCapacity()
+        {
+            Scenario(new int[] { 1, 1, 2, 2 }, new int[] { 2, 2, 3, 3 }, 3, 3);
+        }
+
+        [TestMethod]
+        public void ShouldNotCrash_GivenMaxColor()
+        {
+            Scenario(new int[] { SocksLaundering.MaxColors }, new int[] { SocksLaundering.MaxColors }, 1, 1);
+        }
+
+        [TestMethod]
+        public void ShouldFillAllLaundryCapacity_GivenHugeCapacity()
+        {
+            Scenario(new int[] { 1 }, new int[] { 1, 2, 2, 3, 3, 3, 3 }, int.MaxValue, 4);
+        }
+
+        private void Scenario(int[] cleanSocks, int[] dirtySocks, int laundryCapacity, int expected)
+        {
+            var actual = sut.solution(laundryCapacity, cleanSocks, dirtySocks);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Codility.Tasks/Lesson92/SocksLaundering.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tasks.Lesson92
+{
+    public class SocksLaundering
+    {
+        public const int MaxColors = 50;
+
+        public int solution(int K, int[] C, int[] D)
+        {
+            var Cs = CountSort(C);
+            var Ds = CountSort(D);
+
+            PairSocksByOne(ref K, Cs, Ds);
+            TakeMaximumAmountOfPairs(ref K, Cs, Ds);
+
+            var result = CountPairs(Cs);
+
+            return result;
+        }
+
+        private int CountPairs(int[] array)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                sum += array[i] / 2;
+            }
+
+            return sum;
+        }
+
+        private void PairSocksByOne(ref int K, int[] Cs, int[] Ds)
+        {
+            for (int i = 0; i < MaxColors && K > 0; i++)
+            {
+                if (Cs[i] % 2 == 1 && Ds[i] >= 1) // odd clean socks and at least one dirty exists
+                {
+                    Ds[i] -= 1;
+                    Cs[i] += 1;
+                    K -= 1;
+                }
+            }
+        }
+
+        private void TakeMaximumAmountOfPairs(ref int K, int[] Cs, int[] Ds)
+        {
+            for (int i = 0; i < MaxColors; i++)
+            {
+                var pairedSocks = (Ds[i] >> 1) << 1; // take max even part
+                int take = 0;
+
+                if (K >= pairedSocks)
+                {
+                    take = pairedSocks;
+                }
+                else
+                {
+                    take = (K >> 1) << 1;
+                }
+
+                Ds[i] -= take;
+                Cs[i] += take;
+                K -= take;
+            }
+        }
+
+        private int[] CountSort(int[] array)
+        {
+            var result = new int[MaxColors];
+
+            for (int i = 0; i < array.Length; i++)
+                result[array[i] - 1]++;
+
+            return result;
+        }
+    }
+}
