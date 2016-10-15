fda3eee0007efc3f64476a817c8e6ac7581c84b8
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2016-10-15 16:04:03 +0200

    Lesson02 Arrays OddOccurrencesInArray

---
+++ Codility.Tasks.Tests/Lesson2.Arrays/OddOccurrencesInArrayTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codility.Tasks.Lesson2.Arrays;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tasks.Lesson2.Arrays.Tests
+{
+    [TestClass]
+    public class OddOccurrencesInArrayTests
+    {
+        private OddOccurrencesInArray sut = new OddOccurrencesInArray();
+
+        [TestMethod]
+        public void solutionTest()
+        {
+            var A = new int[] { 9, 3, 9, 3, 9, 7, 9 };
+            var expected = 7;
+            var actual = sut.solution(A);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void solution_ShouldFindCorrectElement_WhenItIsTheMaximum()
+        {
+            var A = new int[] { 1, 3, 1, 3, 5 };
+            var expected = 5;
+            var actual = sut.solution(A);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Codility.Tasks/Lesson2.Arrays/OddOccurrencesInArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tasks.Lesson2.Arrays
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public class OddOccurrencesInArray
+    {
+        public int solution(int[] A)
+        {
+            if (A == null) throw new ArgumentNullException(nameof(A));
+            if (A.Length % 2 == 0) throw new ArgumentException("Array A should be non empty and with odd number of elements");
+            if (A.Length == 1) return A.First();
+
+            // cheat - this makes the time complexity O(NlgN) instead of O(N), but keeps space complexity at O(1)
+            // however, if we use counting sort, we will have O(N) time complexity, but also O(N) space complexiy
+            Array.Sort(A);
+
+            for (int i = 0; i < A.Length - 2; i += 2)
+            {
+                if (A[i] != A[i + 1]) return A[i];
+            }
+
+            return A.Last();
+        }
+    }
+}
