180fc62dd20446041b3ea9934f93efb110b46e8f
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2016-10-19 20:37:44 +0200

    Lesson3 - Tape Equilibrium + tests

---
+++ Codility.Tasks.Tests/Lesson3.TimeComplexity/TapeEquilibriumTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codility.Tasks.Lesson3.TimeComplexity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tasks.Lesson3.TimeComplexity.Tests
+{
+    [TestClass]
+    public class TapeEquilibriumTests
+    {
+        private TapeEquilibrium sut = new TapeEquilibrium();
+
+        [TestMethod]
+        public void solutionTest()
+        {
+            var input = new int[] { 3, 1, 2, 4, 3 };
+            var expected = 1;
+            var actual = sut.solution(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void solution_CanPickFirstElement()
+        {
+            var input = new int[] { 10, 3, 3, 3 };
+            var expected = 1;
+            var actual = sut.solution(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void solution_CanPickLastElement()
+        {
+            var input = new int[] { 3, 3, 3, 10 };
+            var expected = 1;
+            var actual = sut.solution(input);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Codility.Tasks/Lesson3.TimeComplexity/TapeEquilibrium.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tasks.Lesson3.TimeComplexity
+{
+    public class TapeEquilibrium
+    {
+        public int solution(int[] A)
+        {
+            if (A == null) throw new ArgumentNullException(nameof(A));
+            if (A.Length < 2) throw new ArgumentException($"Array {nameof(A)} does not have enough elements");
+
+            var totalSum = A.Sum();
+            var leadingSum = 0;
+            var trailingSum = totalSum;
+            var minDiff = int.MaxValue;
+            var index = 0;
+
+            for (int i = 1; i < A.Length; i++)
+            {
+                leadingSum += A[i - 1];
+                trailingSum -= A[i - 1];
+                var diff = Math.Abs(leadingSum - trailingSum);
+
+                if (diff < minDiff)
+                {
+                    minDiff = diff;
+                    index = i;
+                }
+            }
+
+            return minDiff;
+        }
+    }
+}
