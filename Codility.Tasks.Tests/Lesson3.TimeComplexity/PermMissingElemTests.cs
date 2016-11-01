6008995720267c310346409cbce140edce6054ff
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2016-11-01 19:23:03 +0100

    PermMissingElement - code and test

---
+++ Codility.Tasks.Tests/Lesson3.TimeComplexity/PermMissingElemTests.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codility.Tasks.Lesson3.TimeComplexity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tasks.Lesson3.TimeComplexity.Tests
+{
+    [TestClass]
+    public class PermMissingElemTests
+    {
+        PermMissingElem sut = new PermMissingElem();
+
+        [TestMethod]
+        public void solutionTest()
+        {
+            var input = new int[] { 2, 3, 1, 5 };
+            var actual = sut.solution(input);
+            var expected = 4;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void solution_SingleElement_Test()
+        {
+            var input = new int[] { 2 };
+            var actual = sut.solution(input);
+            var expected = 1;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void solution_MaximumN_Test()
+        {
+            var input = Enumerable.Range(2, 100000 - 1).ToArray();
+            var actual = sut.solution(input);
+            var expected = 1;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Codility.Tasks/Lesson3.TimeComplexity/PermMissingElem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tasks.Lesson3.TimeComplexity
+{
+    public class PermMissingElem
+    {
+        public int solution(int[] A)
+        {
+            if (A == null) throw new ArgumentNullException(nameof(A));
+            if (A.Length == 0) return 1;
+
+            long sum = A.Aggregate(0L, (x, y) => x + y);
+            var arithProgressionSum = (long)(A.Length + 1) * (2 + A.Length) / 2;
+            var missingElement = arithProgressionSum - sum;
+
+            return (int)missingElement;
+        }
+    }
+}
