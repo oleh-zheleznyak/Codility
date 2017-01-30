408dc4f261da91d3fa4a0226d870fc95d7f781b2
Oleh Zheleznyak <oleh.zheleznyak@gmail.com>
2017-01-30 21:35:27 +0100

    Added MissingElement solution with a cheat (HashSet)

---
+++ Codility.Tasks.Tests/Lesson4.CountingElements/MissingElementTests.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Codility.Tasks.Lesson4.CountingElements;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codility.Tasks.Lesson4.CountingElements.Tests
+{
+	[TestClass]
+	public class MissingElementTests
+	{
+		private readonly MissingElement sut = new MissingElement();
+
+		[TestMethod]
+		public void solutionTest()
+		{
+			Scenario(new int[] { 1, 3, 6, 4, 1, 2 }, 5);
+		}
+
+		[TestMethod]
+		public void solution_intMaxValue()
+		{
+			Scenario(new int[] { int.MaxValue }, 1);
+		}
+
+		[TestMethod]
+		public void solution_intMinValue()
+		{
+			Scenario(new int[] { int.MinValue }, 1);
+		}
+
+		[TestMethod]
+		public void solution_intMiniMax()
+		{
+			Scenario(new int[] { int.MinValue, int.MaxValue }, 1);
+		}
+
+		[TestMethod]
+		public void solution_Zero()
+		{
+			Scenario(new int[] { 0 }, 1);
+		}
+
+		[TestMethod]
+		public void solution_ZeroAndMax()
+		{
+			Scenario(new int[] { 0, int.MaxValue }, 1);
+		}
+
+
+		[TestMethod]
+		public void solution_OneAndMax()
+		{
+			Scenario(new int[] { 1, int.MaxValue }, 2);
+		}
+
+		[TestMethod]
+		public void solution_SomeNumbersBiggerThanOne()
+		{
+			Scenario(new int[] { 2, 3, 4, 5, 6 }, 1);
+		}
+
+		[TestMethod]
+		public void solution_SingleRepeatedNumber()
+		{
+			Scenario(Enumerable.Repeat(42, 10).ToArray(), 1);
+		}
+
+		[TestMethod]
+		public void solution_RangeOfNumbers_ShouldReturnNextNumber()
+		{
+			Scenario(Enumerable.Range(-100, 200).ToArray(), 100);
+		}
+
+		[TestMethod]
+		public void solution_RangeWithGap_ShouldDetectGap()
+		{
+			Scenario(Enumerable.Range(-100, 200).Except(new[] { 42 }).ToArray(), 42);
+		}
+
+		private void Scenario(int[] input, int expected)
+		{
+			var actual = sut.solution(input);
+			Assert.AreEqual(expected, actual);
+		}
+	}
+}
---
+++ Codility.Tasks/Lesson4.CountingElements/MissingElement.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Codility.Tasks.Lesson4.CountingElements
+{
+	public class MissingElement
+	{
+		public int solution(int[] A)
+		{
+			if (A == null) throw new ArgumentNullException(nameof(A));
+
+			var spread = GetSpread(A);
+			if (spread.Min > 1) return 1;
+
+			if (spread.Max > byte.MaxValue)
+				return FindSolutionUsingHashSet(A, spread);
+			else
+				return FindSolutionUsingArray(A, spread);
+		}
+
+		private static int FindSolutionUsingHashSet(int[] A, Spread spread)
+		{
+			var hash = new HashSet<int>();
+			for (int i = 0; i < A.Length; i++)
+			{
+				if (!hash.Contains(A[i])) hash.Add(A[i]);
+			}
+			for (int i = 1; i <= spread.Max; i++)
+			{
+				if (!hash.Contains(i)) return i;
+			}
+			return spread.Max + 1;
+		}
+
+
+		private static int FindSolutionUsingArray(int[] A, Spread spread)
+		{
+			var flagArray = FillFlagArray(A, spread);
+
+			for (int i = 0; i < flagArray.Length; i++)
+			{
+				if (!flagArray[i]) return i + spread.Min;
+			}
+
+			return spread.Max + 1;
+		}
+
+		private static bool[] FillFlagArray(int[] A, Spread spread)
+		{
+			var flagArray = new bool[spread.Range()];
+
+			for (int i = 0; i < A.Length; i++)
+			{
+				if (A[i] > 0) flagArray[A[i] - spread.Min] = true;
+			}
+
+			return flagArray;
+		}
+
+		private Spread GetSpread(int[] A)
+		{
+			int min = int.MaxValue, max = 0;
+			for (int i = 0; i < A.Length; i++)
+			{
+				if (A[i] > max) max = A[i];
+				if (A[i] > 0 && A[i] < min) min = A[i];
+			}
+
+			return new Spread(max, min);
+		}
+
+		private struct Spread
+		{
+			public Spread(int max, int min)
+			{
+				Max = max;
+				Min = min;
+			}
+
+			public int Max { get; }
+
+			public int Min { get; }
+
+			public int Range() => Max - Min + 1;
+
+			public override string ToString() => $"[{Min};{Max}]";
+		}
+	}
+}
